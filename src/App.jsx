3f23b52e7dc7310e8c6bbcafbcbeffcf705072c5
Jay Janodia <jay.janodia@gmail.com>
2023-05-15 13:11:34 -0700

    Updated broken project links

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/header/Header";
 import About from "./components/about/About";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
-/* import Experience from "./components/experience/Experience"; */
+import Experience from "./components/experience/Experience";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
@@ -58,6 +58,7 @@ const App = () => {
       <Portfolio />
       <Testimonials />
       <Contact />
+      <Experience />
       <Footer />
       <Theme />
       {showFloatingNav && <FloatingNav />}
