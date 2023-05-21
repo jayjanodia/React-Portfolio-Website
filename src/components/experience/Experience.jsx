7197352b3ffb7af57cbe3a282bbf24922ca60b91
Jay Janodia <jay.janodia@gmail.com>
2023-05-21 14:56:00 -0700

    Added Skills to experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -19,6 +19,55 @@ const Experience = () => {
           </div>
         ))}
       </div>
+      <br />
+      <h2>Skill</h2>
+      <p>
+        Find my Skill Levels for various technologies below on a scale from 1-10
+      </p>
+      <div className="skill" data-aos="fade-up">
+        <li>
+          <h3>Python</h3>
+          <span className="bar">
+            <span className="python"></span>
+          </span>
+        </li>
+        <li>
+          <h3>MySQL</h3>
+          <span className="bar">
+            <span className="mysql"></span>
+          </span>
+        </li>
+        <li>
+          <h3>Java</h3>
+          <span className="bar">
+            <span className="java"></span>
+          </span>
+        </li>
+        <li>
+          <h3>Shell Scripting</h3>
+          <span className="bar">
+            <span className="shell"></span>
+          </span>
+        </li>
+        <li>
+          <h3>HTML</h3>
+          <span className="bar">
+            <span className="html"></span>
+          </span>
+        </li>
+        <li>
+          <h3>CSS</h3>
+          <span className="bar">
+            <span className="css"></span>
+          </span>
+        </li>
+        <li>
+          <h3>JavaScript</h3>
+          <span className="bar">
+            <span className="javascript"></span>
+          </span>
+        </li>
+      </div>
     </section>
   );
 };
